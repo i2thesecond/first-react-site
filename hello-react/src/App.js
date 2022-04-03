e0e47dad94ff78e4953acbce49e6c2679826c148
i2thesecond <gavincrowl@gmail.com>
2022-04-03 11:37:57 -0500

    created component and changed image

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,13 +1,14 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import HomepageImage from './components/HomepageImage'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <HomepageImage />
         <p>
-		My first React website!
+          My first React website!
         </p>
         <a
           className="App-link"
